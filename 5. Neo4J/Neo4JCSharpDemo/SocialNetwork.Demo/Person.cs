d126319cf5007390ac85647cf779a1cc9f1db0d4
davidgreen24 <davidgreen24@gmail.com>
2016-04-02 23:54:06 -0400

    Neo4J added
    2 examples...but I can't seem to connect. May replace with this example
    later:
    https://github.com/neo4j-examples/movies-dotnet-neo4jclient

---
+++ 5. Neo4J/Neo4JCSharpDemo/SocialNetwork.Demo/Person.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+
+namespace SocialNetwork.Demo
+{
+    public class Person
+    {
+        //This is required to make the serializer treat the 'C#' naming style as 'Java' in the DB
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}
---
+++ 5. Neo4J/Neo4JCSharpDemo/SocialNetwork.Demo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SocialNetwork.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Social();
+            s.Setup();
+
+            var friendsOfFriends = s.FriendsOfAFriend(new Person { Name = "Joe" });
+            Console.WriteLine($"Joe's friends (of friends)");
+            foreach (var fof in friendsOfFriends)
+            {
+                Console.WriteLine($"\t{fof.Name}");
+            }
+
+            Console.WriteLine();
+
+            var commonFriends = s.CommonFriends(new Person { Name = "Joe" }, new Person { Name = "Sally" });
+            Console.WriteLine("Joe and Sally's common friends");
+            foreach (var friend in commonFriends)
+            {
+                Console.WriteLine($"\t{friend.Name}");
+            }
+
+            Console.WriteLine();
+
+            var connectingNames = s.ConnectingPaths(new Person { Name = "Joe" }, new Person { Name = "Billy" });
+            Console.WriteLine("Path to Billy");
+            foreach (var name in connectingNames)
+            {
+                Console.WriteLine($"\t{name}");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
