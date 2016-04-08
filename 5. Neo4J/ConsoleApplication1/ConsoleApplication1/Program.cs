f9a674e61b84a8ef6a4784c884f23b32d3c79e85
davidgreen24 <davidgreen24@gmail.com>
2016-04-07 22:57:53 -0400

    Updating assets, walkthru and Ne04J folder arrangement

---
+++ 5. Neo4J/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Neo4jClient;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new GraphClient(new Uri("http://localhost:7474/db/data"), "neo4j", "admin1234");
+            client.Connect();
+
+            var dict = new Dictionary<string, object>();
+
+            string fileName = @"C:\temp\GoTCreate.cql";
+            var lines = File.ReadLines(fileName);
+            string cql = "";
+            foreach (var cypher in lines)
+            {
+                if (String.IsNullOrEmpty(cypher) || cypher.StartsWith(@"//"))
+                {
+                    cql = "";
+                    continue;
+                }
+
+                ////string cypher = "CREATE (:House {name:\"Tyrell\"});";
+
+                cql += cypher;
+                if (cypher.EndsWith(";"))
+                {
+                    Neo4jClient.Cypher.CypherQuery query = new Neo4jClient.Cypher.CypherQuery
+                        (cql, dict, Neo4jClient.Cypher.CypherResultMode.Projection);
+
+                    ((IRawGraphClient)client).ExecuteCypher(query);
+                    Console.WriteLine(cypher);
+                    cql = "";
+                } 
+            }
+
+            Console.WriteLine("Import Complete!");
+
+        }
+    }
+}
